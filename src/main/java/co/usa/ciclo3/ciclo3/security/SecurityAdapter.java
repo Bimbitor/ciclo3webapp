d631bc414d821d01a43765d7b9baa6b5cac133ff
Bimbitor <javijohanro30@gmail.com>
2021-11-27 22:04:44 -0500

    ggg

--- src/main/java/co/usa/ciclo3/ciclo3/security/SecurityAdapter.java
+++ src/main/java/co/usa/ciclo3/ciclo3/security/SecurityAdapter.java
@@ -13,7 +13,7 @@ public class SecurityAdapter extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests(a -> a
-                        .antMatchers("/", "/login", "/error", "/webjars/**", "/api/**").permitAll()
+                        .antMatchers("/", "/error", "/webjars/**", "/api/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(e -> e
